bafd0ab25df2a8e44bef2c500ddf1dd7c2807369
MeruC <36064011+MeruC@users.noreply.github.com>
2022-11-28 21:22:34 +0800

    fixed data types and enum values and missing stuff

--- jgdc_db.sql
+++ jgdc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 08:05 AM
+-- Generation Time: Nov 28, 2022 at 02:13 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -50,10 +50,18 @@ INSERT INTO `accounts` (`Email`, `Password`, `isAdmin`) VALUES
 CREATE TABLE `bookings` (
   `Emp_ID` int(10) NOT NULL,
   `Pat_ID` int(10) NOT NULL,
-  `Service` varchar(30) NOT NULL,
+  `Service` enum('clean','pasta','d_crown','wisdom') NOT NULL,
   `Date` date NOT NULL,
-  `Time` time NOT NULL
+  `Time` time NOT NULL,
+  `state` enum('pending','accepted','completed') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`Emp_ID`, `Pat_ID`, `Service`, `Date`, `Time`, `state`) VALUES
+(1, 1, 'clean', '2022-06-04', '10:00:00', 'pending');
 
 -- --------------------------------------------------------
 
@@ -63,9 +71,16 @@ CREATE TABLE `bookings` (
 
 CREATE TABLE `emp_info` (
   `Emp_ID` int(10) NOT NULL,
-  `Emp_Name` varchar(30) NOT NULL,
-  `Email` varchar(50) NOT NULL
+  `Emp_Name` varchar(60) NOT NULL,
+  `Email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `emp_info`
+--
+
+INSERT INTO `emp_info` (`Emp_ID`, `Emp_Name`, `Email`) VALUES
+(1, 'Juan Dela Cruz', 'test@email.com');
 
 -- --------------------------------------------------------
 
@@ -75,11 +90,18 @@ CREATE TABLE `emp_info` (
 
 CREATE TABLE `patient_info` (
   `Pat_ID` int(10) NOT NULL,
-  `FName` int(20) NOT NULL,
-  `LName` int(20) NOT NULL,
-  `CP_No` int(11) NOT NULL,
-  `Email` varchar(50) NOT NULL
+  `FName` varchar(30) NOT NULL,
+  `LName` varchar(30) NOT NULL,
+  `CP_No` varchar(11) NOT NULL,
+  `Email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patient_info`
+--
+
+INSERT INTO `patient_info` (`Pat_ID`, `FName`, `LName`, `CP_No`, `Email`) VALUES
+(1, 'Mel Vincent', 'Carmelo', '09254976600', 'carmelomelvincent@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -95,7 +117,9 @@ ALTER TABLE `accounts`
 -- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
-  ADD PRIMARY KEY (`Emp_ID`,`Pat_ID`);
+  ADD PRIMARY KEY (`Emp_ID`,`Pat_ID`),
+  ADD KEY `EmpIdFK` (`Emp_ID`),
+  ADD KEY `PatIdFK` (`Pat_ID`);
 
 --
 -- Indexes for table `emp_info`
@@ -108,11 +132,35 @@ ALTER TABLE `emp_info`
 -- Indexes for table `patient_info`
 --
 ALTER TABLE `patient_info`
-  ADD KEY `PatEmFK` (`Email`),
+  ADD PRIMARY KEY (`Pat_ID`),
+  ADD KEY `PatEmFK` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emp_info`
+--
+ALTER TABLE `emp_info`
+  MODIFY `Emp_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `patient_info`
+--
+ALTER TABLE `patient_info`
+  MODIFY `Pat_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `EmpIdFK` FOREIGN KEY (`Emp_ID`) REFERENCES `emp_info` (`Emp_ID`),
+  ADD CONSTRAINT `PatIdFK` FOREIGN KEY (`Pat_ID`) REFERENCES `patient_info` (`Pat_ID`);
 
 --
 -- Constraints for table `emp_info`
