ef1e0a55ff8cfc24cd59104cfa15854f5e4b582d
MeruC <36064011+MeruC@users.noreply.github.com>
2022-12-02 19:57:00 +0800

    update database

--- jgdc_db.sql
+++ jgdc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2022 at 12:13 PM
+-- Generation Time: Dec 02, 2022 at 12:55 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -45,62 +45,39 @@ INSERT INTO `accounts` (`email`, `password`, `permissionLvl`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Table structure for table `account_info`
 --
 
-CREATE TABLE `bookings` (
-  `booking_ID` int(10) NOT NULL,
-  `emp_ID` int(10) NOT NULL,
-  `pat_ID` int(10) NOT NULL,
-  `service` enum('clean','pasta','d_crown','wisdom') NOT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
-  `state` enum('pending','accepted','completed') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `emp_info`
---
-
-CREATE TABLE `emp_info` (
-  `emp_ID` int(10) NOT NULL,
-  `emp_fname` varchar(50) NOT NULL,
-  `emp_lname` varchar(50) NOT NULL,
-  `contactNo` varchar(10) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `emp_info`
---
-
-INSERT INTO `emp_info` (`emp_ID`, `emp_fname`, `emp_lname`, `contactNo`, `email`, `active`) VALUES
-(4, 'Juan', 'Dela Cruz', '9254994400', 'test@email.com', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `patient_info`
---
-
-CREATE TABLE `patient_info` (
-  `pat_ID` int(10) NOT NULL,
-  `pat_fname` varchar(30) NOT NULL,
-  `pat_lname` varchar(30) NOT NULL,
+CREATE TABLE `account_info` (
+  `account_ID` int(10) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
   `contactNo` varchar(10) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `patient_info`
+-- Dumping data for table `account_info`
 --
 
-INSERT INTO `patient_info` (`pat_ID`, `pat_fname`, `pat_lname`, `contactNo`, `email`) VALUES
+INSERT INTO `account_info` (`account_ID`, `fname`, `lname`, `contactNo`, `email`) VALUES
 (3, 'Mel Vincent', 'Carmelo', '9254976600', 'carmelomelvincent@gmail.com'),
 (5, 'Carmelo, Mel Vincent T.', 'Carmelo', '9254976600', 'carmelomelvincent2@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_ID` int(10) NOT NULL,
+  `account_ID` int(10) NOT NULL,
+  `service` enum('clean','pasta','d_crown','wisdom') NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `state` enum('pending','accepted','completed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -113,30 +90,28 @@ ALTER TABLE `accounts`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `account_info`
+--
+ALTER TABLE `account_info`
+  ADD PRIMARY KEY (`account_ID`),
+  ADD KEY `EmFK` (`email`) USING BTREE;
+
+--
 -- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`booking_ID`),
-  ADD KEY `EmpIdFK` (`emp_ID`),
-  ADD KEY `PatIdFK` (`pat_ID`);
-
---
--- Indexes for table `emp_info`
---
-ALTER TABLE `emp_info`
-  ADD PRIMARY KEY (`emp_ID`),
-  ADD KEY `EmpEmFK` (`email`);
-
---
--- Indexes for table `patient_info`
---
-ALTER TABLE `patient_info`
-  ADD PRIMARY KEY (`pat_ID`),
-  ADD KEY `PatEmFK` (`email`);
+  ADD KEY `AccountIdFK` (`account_ID`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `account_info`
+--
+ALTER TABLE `account_info`
+  MODIFY `account_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `bookings`
@@ -145,39 +120,20 @@ ALTER TABLE `bookings`
   MODIFY `booking_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `emp_info`
---
-ALTER TABLE `emp_info`
-  MODIFY `emp_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `patient_info`
---
-ALTER TABLE `patient_info`
-  MODIFY `pat_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `account_info`
+--
+ALTER TABLE `account_info`
+  ADD CONSTRAINT `EmFK` FOREIGN KEY (`email`) REFERENCES `accounts` (`email`);
 
 --
 -- Constraints for table `bookings`
 --
 ALTER TABLE `bookings`
-  ADD CONSTRAINT `EmpIdFK` FOREIGN KEY (`emp_ID`) REFERENCES `emp_info` (`emp_ID`),
-  ADD CONSTRAINT `PatIdFK` FOREIGN KEY (`pat_ID`) REFERENCES `patient_info` (`pat_ID`);
-
---
--- Constraints for table `emp_info`
---
-ALTER TABLE `emp_info`
-  ADD CONSTRAINT `EmpEmFK` FOREIGN KEY (`email`) REFERENCES `accounts` (`email`);
-
---
--- Constraints for table `patient_info`
---
-ALTER TABLE `patient_info`
-  ADD CONSTRAINT `PatEmFK` FOREIGN KEY (`email`) REFERENCES `accounts` (`email`);
+  ADD CONSTRAINT `AccountIdFK` FOREIGN KEY (`account_ID`) REFERENCES `account_info` (`account_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
