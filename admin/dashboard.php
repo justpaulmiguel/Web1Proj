539a8eac5f3e757a5c1712856f0641ab58cad3b9
justpaulmiguel <111945104+justpaulmiguel@users.noreply.github.com>
2022-12-04 22:19:40 +0800

    Update dashboard.php
    Naglagay lang ng dummy para sa dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,7 +2,63 @@
 
 
 <main>
-	<h1>Hello World!</h1>
+	<h1>Dashboard</h1>
+
+	<ol>
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+		
+		<li>09/01/2022 &nbsp;&nbsp;&nbsp;&nbsp; 9:00AM - 9:30PM &nbsp;&nbsp;&nbsp;&nbsp; Squarepants Spongebob
+			&nbsp;&nbsp;&nbsp;&nbsp; Oral Propalaxyis &nbsp;&nbsp;&nbsp;&nbsp; Joseph Joestar 
+			&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" value="Completed">
+		</li>
+
+
+		
+
+	</ol>
 </main>
 
 
