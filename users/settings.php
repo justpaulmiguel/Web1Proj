f63a72a7cb5edc4785a3dbd8dae3bfcc8ca2bcc8
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-05 09:41:13 +0800

    Remove change email section

--- users/settings.php
+++ users/settings.php
@@ -3,27 +3,7 @@
 require("./partials/head.php");
 ?>
 <main>
-  <h2>Change Email</h2>
-  <section class="change-email-wrapper">
-    <form action="/path/to/backend" method="post">
-      <div class="input-wrapper">
-        <label for="inputCurrentEmail">Current Email:</label>
-        <input type="text" max="100" name="currentEmail" id="inputCurrentEmail" required />
-      </div>
 
-      <div class="input-wrapper">
-        <label for="inputNewEmail">New Email:</label>
-        <input type="text" max="100" name="newEmail" id="inputNewEmail" required />
-      </div>
-
-      <div class="input-wrapper">
-        <label for="inputPassword">Password:</label>
-        <input type="password" max="100" name="password" id="inputPassword" required />
-      </div>
-
-      <button type="submit">Change</button>
-    </form>
-  </section>
   <h2>Change Password</h2>
   <section class="change-password-wrapper">
     <form action="/path/to/backend" method="post">
