73b0da3a33966d254f1e0e01fc36b5a95dce0f77
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-05 09:47:22 +0800

    Update login redirect

--- php/login.php
+++ php/login.php
@@ -1,83 +1,80 @@
 <?php
 session_start();
 require "dbConnect.php";
-if(isset($_SESSION["emailLogin"]) && isset($_SESSION["passLogin"])){
+if (isset($_SESSION["emailLogin"]) && isset($_SESSION["passLogin"])) {
     header("Location: menu.php");
     exit();
-}	
+}
 $email = $_POST["emailLogin"];
 $pass = $_POST["passLogin"];
 $query = "SELECT * FROM accounts WHERE email='$email'";
-$result = mysqli_query($conn,$query);
+$result = mysqli_query($conn, $query);
 $count =  mysqli_num_rows($result);
 
 # ACCOUNT VALIDATION
-if($count==1){
+if ($count == 1) {
     $query = "SELECT password FROM accounts WHERE email='$email'";
-    $result = mysqli_query($conn,$query);
+    $result = mysqli_query($conn, $query);
     $passHash = mysqli_fetch_array($result);
 
-    if(password_verify($pass, $passHash[0])) {
-            
+    if (password_verify($pass, $passHash[0])) {
+
         $_SESSION["email"] = $email;
         $_SESSION["password"] = $pass;
         $query = "SELECT permissionLvl FROM accounts WHERE email='$email'and password='$pass'";
-        $result = mysqli_query($conn,$query);
+        $result = mysqli_query($conn, $query);
         $permissionLvl = mysqli_fetch_array($result);
         # PERMISSION LEVEL CHECK
-        if($permissionLvl[0]>0)  {
+        if ($permissionLvl[0] > 0) {
             $_SESSION["permissionLvl"] = $permissionLvl[0];
-            ?>
+?>
             <script>
                 Swal.fire({
-                icon: 'success',
-                text: 'Success!',
-                confirmButtonColor: '#e05c2a'
+                    icon: 'success',
+                    text: 'Success!',
+                    confirmButtonColor: '#e05c2a'
                 }).then(function() {
                     window.location = "admin/dashboard.php";
                 });
             </script>");
-            <?php
+        <?php
         } else {
             $_SESSION["permissionLvl"] = $permissionLvl[0];
-            ?>
+        ?>
             <script>
                 Swal.fire({
-                icon: 'success',
-                text: 'Success!',
-                confirmButtonColor: '#e05c2a'
+                    icon: 'success',
+                    text: 'Success!',
+                    confirmButtonColor: '#e05c2a'
                 }).then(function() {
-                    window.location = "users/user.php";
+                    window.location = "users/dashboard.php";
                 });
             </script>");
-            <?php
+        <?php
         }
-        
     } else {
         header("refresh: 0; url=../index.php");
         ?>
         <script>
             Swal.fire({
-            icon: 'error',
-            text: 'PASSWORD IS INCORRECT!',
-            confirmButtonColor: '#e05c2a'
+                icon: 'error',
+                text: 'PASSWORD IS INCORRECT!',
+                confirmButtonColor: '#e05c2a'
             })
         </script>");
-        <?php
+    <?php
     }
-
-}
-else{
+} else {
     header("refresh: 0; url=../index.php");
     ?>
     <script>
         Swal.fire({
-        icon: 'error',
-        text: "ACCOUNT DOESN'T EXIST!",
-        confirmButtonColor: '#e05c2a'
+            icon: 'error',
+            text: "ACCOUNT DOESN'T EXIST!",
+            confirmButtonColor: '#e05c2a'
         })
     </script>");
-    <?php
+<?php
 }
 mysqli_close($conn);
 ?>
