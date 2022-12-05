ca904702f0c1cd5e9a04a0e84c3d775be980067e
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-05 16:27:31 +0800

    Add settings feature

--- admin/settings.php
+++ admin/settings.php
@@ -1,4 +1,5 @@
 <?php require("partials/head.php") ?>
+
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -6,7 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $newPass = $_POST["newPassword"];
     $confirmNewPass = $_POST["confirmNewPassword"];
 
-
+    $toggle = false;
+    // post values validation
     if (
         strlen($currentPass) == 0
         || strlen($newPass) == 0
@@ -14,15 +16,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ) {
 
         echo showModalError("Incomplete details");
+        $toggle = true;
     }
 
 
-    if (!strcmp($newPass, $confirmNewPass)) {
-        echo  showModalError("New passwords are not the same!");
+    if (strcmp($newPass, $confirmNewPass) != 0) {
+        echo  showModalError("New passwords are not the same!" . $currentPass . $newPass);
+        $toggle = true;
     }
 
-    // Actual query
+    if (!$toggle) {
+        // Query time
+        // check if password is the same
+        require('../php/dbConnect.php');
+        $query = sprintf("SELECT password FROM accounts WHERE email='%s' LIMIT 1;", $_SESSION['email']);
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) {
+            $passHash = mysqli_fetch_array($result)[0];
 
+            if (password_verify(
+                $currentPass,
+                $passHash
+            )) {
+                $passHash = password_hash($newPass, PASSWORD_DEFAULT);
+                // perform changing of password
+                $query = sprintf(
+                    "UPDATE  accounts SET password='%s' WHERE email='%s' LIMIT 1;",
+                    $passHash,
+                    $_SESSION['email']
+                );
+                if (mysqli_query($conn, $query)) {
+                    echo showModalError("Updated Successfully");
+                } else {
+                    echo showModalError("Error updating of Password");
+                }
+            } else {
+                echo showModalError("Current Password is incorrect desu." . $currentPass);
+            }
+        } else {
+            echo showModalError("Sql failed me lol");
+        }
+    }
 }
 ?>
 
@@ -32,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="input-wrapper">
                 <label for="inputNewPassword">New Password:</label>
-                <input type="password" max="100" name="newPassword" id="inputNewPassword" />
+                <input type="password" max="100" name="newPassword" id="inputNewPassword" required />
             </div>
 
             <div class="input-wrapper">
