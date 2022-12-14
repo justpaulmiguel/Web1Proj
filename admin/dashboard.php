f890184bd46530bf78a891c70dc333e49817e9d9
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-14 19:41:38 +0800

    Adjust dashboard display

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -21,7 +21,7 @@ if (isset($_SESSION['flash_message'])) {
 		<?php
 		require("../php/dbConnect.php");
 		$date = date('Y-m-d');
-		$query = "SELECT booking_ID, account_ID, state,  DATE_FORMAT(date,'%W, %M %d %Y') as date , TIME_FORMAT(time, '%l:%i %p') as time  FROM bookings WHERE state='accepted' AND date='$date'";
+		$query = "SELECT booking_ID, account_ID, state,   TIME_FORMAT(time, '%l:%i %p') as time  FROM bookings WHERE state='accepted' AND date='$date' ORDER BY time ASC ";
 		$result = mysqli_query($conn, $query);
 		if (mysqli_num_rows($result) > 0) {
 		?>
@@ -38,14 +38,12 @@ if (isset($_SESSION['flash_message'])) {
 							<th>Last Name</th>
 							<th>Contact</th>
 							<th>Time</th>
-							<th>Date</th>
-							<th>State</th>
+							<th>Mark As</th>
 						</tr>
 						<?php while ($row = mysqli_fetch_assoc($result)) {
 							$bookID = $row["booking_ID"];
 							$accID = $row['account_ID'];
 							$time = $row['time'];
-							$date = $row['date'];
 							$query1 = "SELECT * FROM account_info WHERE account_id='$accID'";
 							$result1 = mysqli_query($conn, $query1);
 							if (mysqli_num_rows($result1) > 0) {
@@ -57,12 +55,14 @@ if (isset($_SESSION['flash_message'])) {
 									echo "<tr><td> "
 										. $fname . "</td><td>"
 										. $lname . "</td><td>"
-										. "<a href=mailto:$email>" . $email . "</a>" .
+										.
 										"<p>0" .
-										$contactNo . "</p>" . "</td><td>"
+										$contactNo . "</p>" . "<a href=mailto:$email>" . $email . "</a>" . "</td><td>"
 										. $time . "</td><td>"
-										. $date . "</td><td>"
-										. "<button class='form-btn' type=button value='$bookID'>Completed</button>";
+										. "<button class='form-btn completed-btn' type=button value='$bookID'>Completed</button>
+										 <button class='form-btn missed-btn' type=button value='$bookID'>Missed</button>
+										
+										";
 								}
 							}
 						}
@@ -86,11 +86,33 @@ if (isset($_SESSION['flash_message'])) {
 		const form = document.querySelector("#complete-appointment");
 		form.addEventListener('submit', (e) => {
 			e.preventDefault();
-
 		})
-		const btns = [...document.querySelectorAll(".form-btn")];
-		btns.forEach(btn => {
-			btn.addEventListener("click", (e) => {
+		const handleMissedBtnClick = (btn) => {
+			btn.addEventListener('click', (e) => {
+				e.preventDefault();
+				Swal.fire({
+					title: 'Are you sure?',
+					text: "Do you want to mark this appointment as missed?",
+					icon: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Yes'
+				}).then((result) => {
+					if (result.isConfirmed) {
+						const stateChangeInput = document.createElement("input");
+						stateChangeInput.setAttribute("type", "hidden");
+						stateChangeInput.setAttribute("value", btn.value);
+						stateChangeInput.setAttribute("name", "missed");
+						form.appendChild(stateChangeInput);
+						form.submit();
+					}
+				})
+			})
+
+		};
+		const handleCompletedBtnClick = (btn) => {
+			btn.addEventListener('click', (e) => {
 				e.preventDefault();
 				Swal.fire({
 					title: 'Are you sure?',
@@ -105,13 +127,18 @@ if (isset($_SESSION['flash_message'])) {
 						const stateChangeInput = document.createElement("input");
 						stateChangeInput.setAttribute("type", "hidden");
 						stateChangeInput.setAttribute("value", btn.value);
-						stateChangeInput.setAttribute("name", "stateChange");
+						stateChangeInput.setAttribute("name", "completed");
 						form.appendChild(stateChangeInput);
 						form.submit();
 					}
 				})
 			})
-		})
+
+		};
+		const completedBtns = [...document.querySelectorAll(".completed-btn")];
+		const missedBtns = [...document.querySelectorAll(".missed-btn")];
+		completedBtns.map(handleCompletedBtnClick);
+		missedBtns.map(handleMissedBtnClick);
 	}
 </script>
 
--- admin/updateDashboard.php
+++ admin/updateDashboard.php
@@ -2,14 +2,22 @@
 session_start();
 
 require("../php/dbConnect.php");
-if (isset($_REQUEST['stateChange'])) {
+if (isset($_REQUEST['completed'])) {
 
-	$bookID1 = $_POST["stateChange"];
+	$bookID1 = $_POST["completed"];
 	$query2 = "UPDATE `bookings` SET `state` = 'completed' WHERE booking_ID='$bookID1'";
 	$result2 = mysqli_query($conn, $query2);
 	mysqli_query($conn, $query2);
 	mysqli_close($conn);
-	$_SESSION['flash_message'] = "Update Successful";
+	$_SESSION['flash_message'] = "Update Successful. Appointment is complete.";
+} else if (isset($_REQUEST['missed'])) {
+
+	$bookID1 = $_POST["missed"];
+	$query2 = "UPDATE `bookings` SET state` = 'cancelled',`note` = 'Did not show up' WHERE booking_ID='$bookID1'";
+	$result2 = mysqli_query($conn, $query2);
+	mysqli_query($conn, $query2);
+	mysqli_close($conn);
+	$_SESSION['flash_message'] = "Update Successful. Appointment is cancelled.";
 } else {
 	$_SESSION['flash_message'] = "Update Unsucessful";
 }
