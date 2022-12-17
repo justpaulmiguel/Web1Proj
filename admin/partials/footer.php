d51bfe34bc094c31bff1e177c813561689abbd4d
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-17 19:38:43 +0800

    Adjust footer dummy data

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -11,10 +11,10 @@
         <div class="contacts-wrapper">
             <div class="logo footer-place-holder-logo"><span>L</span></div>
             <div class="contact-details-wrapper">
-                <p>Address: Sanfernando, Pampanga</p>
-                <p>Tel/CP no.: 09xxxxxxxxx</p>
-                <p>Facebook: facebook.com</p>
-                <p>Email: pauljustine.miguel.a@bulsu.edu.ph</p>
+                <p>Address: San Pedro, San Simon, Pampanga</p>
+                <p>Tel/CP no.: 09876543210</p>
+                <p>Facebook: <a href="https://facebook.com/jgdc">Joseph Galang Dental Clinic</a> </p>
+                <p>Email: sample@email.com</p>
             </div>
         </div>
     </div>
