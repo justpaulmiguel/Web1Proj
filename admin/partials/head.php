9a0a65ae88a097b3a286bc8bc64c3a87590e97e3
Dyann@Alicelise <wilhelmjanmapogi@gmail.com>
2022-12-18 15:55:05 +0800

    Add page icon

--- admin/partials/head.php
+++ admin/partials/head.php
@@ -30,6 +30,8 @@ require_once("../php/functions.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $title ?></title>
+    <!-- page icon -->
+    <link rel="icon" type="image/x-icon" href="../../logo.png">
     <!-- Google Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- Google Fonts -->
--- patient/partials/head.php
+++ patient/partials/head.php
@@ -19,6 +19,8 @@ require("../php/functions.php");
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- page icon -->
+    <link rel="icon" type="image/x-icon" href="../../logo.png">
     <!-- Google Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- Google Fonts -->
