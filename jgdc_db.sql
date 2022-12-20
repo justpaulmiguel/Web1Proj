08bcf3a160cc1d049545eff88831f68718026a5c
MeruC <36064011+MeruC@users.noreply.github.com>
2022-12-20 15:10:12 +0800

    database, updated

--- jgdc_db.sql
+++ jgdc_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2022 at 11:24 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Dec 20, 2022 at 08:08 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,16 +31,15 @@ CREATE TABLE `accounts` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `permissionLvl` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `accounts`
 --
 
 INSERT INTO `accounts` (`email`, `password`, `permissionLvl`) VALUES
-('carmelomelvincent@gmail.com', '$2y$10$zlKOHK1qQ1Os/h8MAi/fFO3xCCpzUKfJG9QIJp7XQsK5iKvQAtUSq', 0),
-('test2@gmail.com', '$2y$10$GHWEPjdVMcL7GMttBXIuoOU8FAegaFyBS4/bur7prYkzJ9.bVI6ai', 2),
-('test3@gmail.com', '$2y$10$L45ACn9xQKA4w4Z8vFDVXubg3nSiAuyj/uWS8ERiz7ol8g03x8F1q', 0);
+('adminPassword0@gmail.com', '$2y$10$F6OFyrpK7Z4HrGTwim059OW30HxZBY1AVD0fyD5Tn.e4F49uwDFga', 2),
+('userPassword0@gmail.com', '$2y$10$kvNL7gw5H17J./Ocp6d7f.amyMRpCgKnmL4uyhO7zQYwMsp3xzFQO', 0);
 
 -- --------------------------------------------------------
 
@@ -54,16 +53,15 @@ CREATE TABLE `account_info` (
   `lname` varchar(30) NOT NULL,
   `contactNo` varchar(10) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `account_info`
 --
 
 INSERT INTO `account_info` (`account_ID`, `fname`, `lname`, `contactNo`, `email`) VALUES
-(3, 'Mel Vincent', 'Carmelo', '9254976600', 'carmelomelvincent@gmail.com'),
-(6, 'John', 'Doe', '9254976600', 'test2@gmail.com'),
-(10, 'John', 'Doe', '9254976600', 'test3@gmail.com');
+(1, 'John', 'Doe', '9123456789', 'adminPassword0@gmail.com'),
+(2, 'Taro', 'Yamada', '9876543219', 'userPassword0@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -77,23 +75,10 @@ CREATE TABLE `bookings` (
   `service` enum('clean','pasta','d_crown','wisdom') NOT NULL,
   `date` date NOT NULL,
   `time` time NOT NULL,
-  `state` enum('pending','accepted','completed','declined','cancelled') NOT NULL,
+  `state` enum('pending','accepted','past','completed','declined','cancelled') NOT NULL,
   `branch` enum('s_simon','mexico') NOT NULL,
   `note` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `bookings`
---
-
-INSERT INTO `bookings` (`booking_ID`, `account_ID`, `service`, `date`, `time`, `state`, `branch`, `note`) VALUES
-(6, 3, 'clean', '2022-12-07', '21:30:00', 'accepted', 's_simon', ''),
-(8, 3, 'pasta', '2022-12-09', '09:00:00', 'accepted', 'mexico', ''),
-(9, 3, 'd_crown', '2022-12-14', '12:30:00', 'accepted', 's_simon', ''),
-(10, 3, 'wisdom', '2022-12-21', '12:30:00', 'accepted', 'mexico', ''),
-(11, 3, 'clean', '2023-01-11', '09:00:00', 'accepted', 's_simon', ''),
-(12, 3, 'pasta', '2022-12-01', '09:00:00', 'declined', 'mexico', 'unnavailable'),
-(13, 3, 'clean', '2022-12-06', '22:06:23', 'accepted', 's_simon', '');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -127,13 +112,13 @@ ALTER TABLE `bookings`
 -- AUTO_INCREMENT for table `account_info`
 --
 ALTER TABLE `account_info`
-  MODIFY `account_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `account_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `booking_ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Constraints for dumped tables
